4b10e6ab6a485b7077b26e211cf478d4d5d142a0
L-aaal <lll.zt1212@gmail.com>
2025-04-21 08:26:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -69,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(orderData),
+                mode: "no-cors" //嘉眉給的程式，一定要留著！！！！！！
             })
                 .then((response) => response.text())
                 .then((data) => {
